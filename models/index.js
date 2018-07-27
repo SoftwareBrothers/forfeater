7d8199075b4c062181cedac072ffb21d592a14bf
Marek Firlejczyk <marek.firlejczyk@rst-it.com>
2018-07-27 10:24:08 +0200

    Improve models index connection - use dotenv

--- models/index.js
+++ models/index.js
@@ -1,37 +1,42 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
+var fs = require('fs');
+var path = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || 'production';
+var config = require('../config/config.js')[env];
+var db = {};
 
-if (config.use_env_variable) {
-  var url = 'postgres://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME;
-  var sequelize = new Sequelize(url, config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+var sequelize = new Sequelize(config);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+    fs
+      .readdirSync(__dirname)
+      .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+      })
+      .forEach(file => {
+        var model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+      });
+
+    Object.keys(db).forEach(modelName => {
+      if (db[modelName].associate) {
+        db[modelName].associate(db);
+      }
+    });
+
+    db.sequelize = sequelize;
+    db.Sequelize = Sequelize;
+
+    module.exports = db;
   })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
