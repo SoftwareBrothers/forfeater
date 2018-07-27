a4bed6fdcf2f9d0bee4be5e53e41a2f6a7caad5e
Paweł Lorenc <pawel.lorenc@rst-it.com>
2018-07-27 10:29:43 +0200

    WIP

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,22 +1,79 @@
 var db = require('../models');
 var Vendor = db.vendor;
 
-exports.vendor_list = function (req, res) {
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
+exports.list = function (req, res) {
     Vendor.findAll().then(vendors => {
         res.json(vendors);
     })
 };
 
-exports.vendor_show = function (req, res) {
+exports.show = function (req, res) {
     Vendor.findById(req.params.id).then(vendor => {
         res.json(vendor);
     })
 };
 
-exports.vendor_create_form = function (req, res) {
-    res.send("vendor create form");
-};
+// exports.create = function (req, res) {
+//     res.send("vendor create form");
+// };
 
-exports.vendor_create_action = function (req, res) {
-    res.send("vendor create action (post)");
-};
+// exports.store = function (req, res) {
+//     res.send("vendor create action (post)");
+// };
+
+// Handle Genre create on POST.
+exports.store =  [
+   
+    // Validate that the name field is not empty.
+    body('name', 'Name required').isLength({ min: 1 }).trim(),
+
+    // Sanitize (trim and escape) the name field.
+    sanitizeBody('name').trim().escape(),
+    sanitizeBody('url').trim().escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a genre object with escaped and trimmed data.
+        var vendor = new Vendor(
+          { name: req.body.name },
+          { url: req.body.url }
+        );
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            // res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+            res.json({errors: errors.array(), body: req.body});
+        return;
+        }
+        else {
+            // Data from form is valid.
+            // Check if Genre with same name already exists.
+            Vendor.findOne({ 'name': req.body.name })
+                .exec( function(err, found_vendor) {
+                     if (err) { return next(err); }
+
+                     if (found_vendor) {
+                         // Genre exists, redirect to its detail page.
+                         res.json('exists!');
+                     }
+                     else {
+
+                         vendor.save(function (err) {
+                           if (err) { return next(err); }
+                           // Genre saved. Redirect to genre detail page.
+                           res.json('success');
+                         });
+
+                     }
+
+                 });
+        }
+    }
+];
--- routes/vendors.js
+++ routes/vendors.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 var vendor_controller = require('../controllers/vendorController');
 
-router.get('/', vendor_controller.vendor_list);
-router.get('/create', vendor_controller.vendor_create_form);
-router.get('/:id', vendor_controller.vendor_show);
+router.get('/', vendor_controller.list);
+router.get('/:id', vendor_controller.show);
+
+// router.get('/create', vendor_controller.create);
+router.post('/create', vendor_controller.store);
 
 module.exports = router;
