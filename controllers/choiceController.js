43af7cdebc1872d961cfd0835b3ab5e10926c6fd
Piotr Wilczyński <piotr.wilczynski@rst-it.com>
2018-07-30 14:11:51 +0200

    enable changing choice

--- controllers/choiceController.js
+++ controllers/choiceController.js
@@ -29,9 +29,7 @@ exports.show = function (req, res) {
 }
 
 exports.store =  [
-   
     // Validate that the name field is not empty.
-    body('orderId', 'OrderId required').isLength({ min: 1 }).trim(),
     body('userId', 'UserId required').isLength({ min: 1 }).trim(),
     body('productId', 'ProductId required').isLength({ min: 1 }).trim(),
 
@@ -40,13 +38,12 @@ exports.store =  [
 
     // Process request after validation and sanitization.
     (req, res, next) => {
-
         // Extract the validation errors from a request.
         const errors = validationResult(req);
 
         // Create a genre object with escaped and trimmed data.
         var model = new Choice({
-            orderId: req.body.orderId,  
+            orderId: req.params.orderId,
             userId: req.body.userId,
             productId: req.body.productId
         });
@@ -55,28 +52,21 @@ exports.store =  [
             res.status(422).json({status: 'fail', errors: errors.array()});
         }
         else {
-        
-            Choice.findOne({ where: {'orderId': req.body.orderId, 'userId': req.body.userId }})
-                .then( found_model => { 
-                     if (found_model) {
-                         res.status(409).json({status: 'fail', data: 'Choice already exists!'});
-                     }
-                     else {
-                        model.save()
-                        .then(() => {
-                            res.status(200).json({status: 'success'});
-                        })
-                        .catch(function (err) {
-                            if (err instanceof db.Sequelize.ForeignKeyConstraintError) {
-                                res.status(501).json({status: 'status', error: err.message});
-                            } else {
-                                res.status(500).json({status: 'status', error: err.message});
-                            }
-                            console.error(err);
-                          })
-                     }
-
-                 });
+            var choice = Choice.findOrCreate({
+                where: {
+                    'orderId': req.params.orderId,
+                    'userId': req.body.userId,
+                    'productId': req.body.productId
+                }
+            }).then( choice => {
+                res.status(200).json(choice);
+            }).catch(function (error) {
+                if (error instanceof db.Sequelize.ForeignKeyConstraintError) {
+                    res.status(501).json({status: 'status', error: error.message});
+                } else {
+                    res.status(500).json({status: 'status', error: error.message});
+                }
+            });
         }
     }
 ];
