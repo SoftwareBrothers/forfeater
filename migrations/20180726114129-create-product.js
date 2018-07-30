aafabd5f674b6d0848cc4d5fcdc97be9ca20c640
pwilczynski <pwilczynski@protonmail.com>
2018-07-30 12:14:50 +0200

    products.active column
    used to determine if product should be shown for users while putting an order

--- migrations/20180726114129-create-product.js
+++ migrations/20180726114129-create-product.js
@@ -8,6 +8,11 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      active: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          defaultValue: 1,
+      },
       name: {
         type: Sequelize.STRING
       },
