eb40a55b82922387a46bd41e49d90ac7b6c3fe91
Piotr Wilczyński <piotr.wilczynski@rst-it.com>
2018-07-30 14:11:33 +0200

    new endpoint for choices
    PUT /orders/:orderId/choices

--- routes/orders.js
+++ routes/orders.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var order_controller = require('../controllers/orderController');
+var choice_controller = require('../controllers/choiceController');
 
 router.get('/', order_controller.list);
 router.get('/:id', order_controller.show);
 
 router.post('/create', order_controller.store);
+
+router.put('/:orderId/choices', choice_controller.store);
 
 module.exports = router;
