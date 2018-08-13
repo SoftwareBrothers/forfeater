a0dd52ff821b64d290858695c9e9d6db6082c044
Marek Firlejczyk <marek.firlejczyk@rst-it.com>
2018-08-13 10:57:21 +0200

    Cross acces update

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
