c4d598b2dc56ebd95e84d3cb526e2ef6f7fabd0d
Paweł Lorenc <pawlakzn@gmail.com>
2018-09-17 23:51:54 +0200

    more users in seeders

--- seeders/20180726112905-user.js
+++ seeders/20180726112905-user.js
@@ -6,26 +6,41 @@ module.exports = {
       "users",
       [
         {
-          firstName: "Piotr",
-          lastName: "Wilczyński",
-          role: "admin",
-          email: "piotr.wilczynski@rst-it.com",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          // password is test
-          password:
-            "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4"
+          firstName: "Piotr", lastName: "Wilczyński", role: "admin", email: "piotr.wilczynski@rst-it.com", createdAt: new Date(), updatedAt: new Date(),
+          password: "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4" // password is test
         },
         {
-          firstName: "Paweł",
-          lastName: "Lorenc",
-          role: "admin",
-          email: "pawel.lorenc@rst-it.com",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          // password is test
-          password:
-            "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4"
+          firstName: "Paweł", lastName: "Lorenc", role: "admin", email: "pawel.lorenc@rst-it.com", createdAt: new Date(), updatedAt: new Date(),       
+          password: "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4" // password is test
+        },
+        {
+          firstName: "Marek", lastName: "Firlejczyk", role: "admin", email: "marek.firlejczyk@rst-it.com", createdAt: new Date(), updatedAt: new Date(),       
+          password: "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4" // password is test
+        },
+        {
+          firstName: "Wojciech", lastName: "Krysiak", role: "admin", email: "wojciech.krysiak@rst-it.com", createdAt: new Date(), updatedAt: new Date(),       
+          password: "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4" // password is test
+        },
+        {
+          firstName: "Marta", lastName: "Wodzicka", role: "admin", email: "marta.wodiczka@rst-it.com", createdAt: new Date(), updatedAt: new Date(),       
+          password: "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4" // password is test
+        },
+
+        {
+          firstName: "Katarzyna", lastName: "Heltman", role: "user", email: "katarzyna.heltman@rst-it.com", createdAt: new Date(), updatedAt: new Date(),       
+          password: "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4" // password is test
+        },
+        {
+          firstName: "Wojciech", lastName: "Pawlinów", role: "user", email: "wojciech.pawlinow@rst-it.com", createdAt: new Date(), updatedAt: new Date(),       
+          password: "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4" // password is test
+        },
+        {
+          firstName: "Szymon", lastName: "Półtorak", role: "user", email: "szymon.poltorak@rst-it.com", createdAt: new Date(), updatedAt: new Date(),       
+          password: "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4" // password is test
+        },
+        {
+          firstName: "Arkadiusz", lastName: "Pęcherz", role: "user", email: "arkadiusz.pecherz@rst-it.com", createdAt: new Date(), updatedAt: new Date(),       
+          password: "$argon2i$v=19$m=4096,t=3,p=1$vh121pIrNfQQIWNrR0vGsQ$zeZPx5uxxkoPgBH70P+p7Iciltdghg7EjNC8rJHOCs4" // password is test
         }
       ],
       {}
