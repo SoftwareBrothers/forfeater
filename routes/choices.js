5935b2bd4ee4baf99a69464ce76fad158b054fde
Paweł Lorenc <pawel.lorenc@rst-it.com>
2019-01-19 00:16:12 +0100

    removed duplicated routes

--- routes/choices.js
+++ routes/choices.js
@@ -3,9 +3,6 @@ var router = express.Router();
 
 var choice_controller = require('../controllers/choiceController');
 
-router.get('/', choice_controller.list);
-router.get('/:id', choice_controller.show);
-
 module.exports = function (app) {
     router.get('/', choice_controller.list);
     router.get('/:id', choice_controller.show);
